aae2440d3e8fc48cb5096ffdb0a4d1ce7ec95741
hn634 <kagoshimagasukidesu@gmail.com>
2026-03-05 20:14:19 +0900

    アカウント作成画面の追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import ToDoForm from "./components/ToDoForm";
 import ToDoList from "./components/ToDoList";
+import SignUpForm from "./components/SignUpForm";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -11,10 +12,24 @@ function App() {
     setTodos((prev) => [...prev, task.trim()]);
   };
 
+  const appStyle = {
+    maxWidth: "600px",
+    margin: "0 auto",
+    padding: "20px",
+    boxSizing: "border-box",
+  };
+
   return (
-    <div style={{ padding: 20 }}>
+    <div style={appStyle}>
       <Header />
+
+      {/* サインアップフォーム */}
+      <SignUpForm />
+
+      {/* Todoフォーム */}
       <ToDoForm addTodo={addTodo} />
+
+      {/* Todoリスト */}
       <ToDoList todos={todos} />
     </div>
   );
